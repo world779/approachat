161c76c4a656d2da6b3913bd69608847bb89a10f
Morinaga <s0820hika@gmail.com>
2020-12-13 11:36:11 +0900

    create login system

--- public/app.js
+++ public/app.js
@@ -5,49 +5,87 @@ const IAM = {
   id: "",
   room: "",
   token: "",
-  isEnter : false
-}
+  isEnter: false,
+};
+
+var express = require("express");
+var router = express.Router();
+var pg = require("pg");
+
+var pool = new pg.Pool({
+  database: "chatdb",
+  user: "postgres", //ユーザー名はデフォルト以外を利用している人は適宜変更してください。
+  password: "Arai25", //PASSWORDにはPostgreSQLをインストールした際に設定したパスワードを記述。
+  host: "localhost",
+  port: 5432,
+});
+
+/*ログイン画面 */
+router.post("/", function (req, res, next) {
+  /*入力した内容をDBから探して一致したらログインさせたい*/
+  pool.connect(function (err, client) {
+    if (err) {
+      console.log(err);
+    } else {
+      var compare =
+        "SELECT * FROM Web_user WHERE mail = '" +
+        req.body.mail +
+        "' AND pass = '" +
+        req.body.pass +
+        "'";
+      client.query(compare, function (err, result) {
+        if (err || result.rowCount == 0) {
+          console.log(err);
+          res.render("error-login");
+        } else {
+          res.render("index");
+          console.log(result);
+        }
+      });
+    }
+  });
+});
 
 // C04. server_to_clientイベント・データを受信する
 socket.on("s2c_msg", function (data) {
   appendMsg(data.msg);
 });
 
-socket.on("s2c_leave", function(data) {
+socket.on("s2c_leave", function (data) {
   appendMsg(data.msg);
   $(`#${data.id}`).remove();
 });
 
-socket.on("connect",function(data){
-    console.log(data);
+socket.on("connect", function (data) {
+  console.log(data);
 });
 
-socket.on("token", function(data){
+socket.on("token", function (data) {
   IAM.token = data.token;
   IAM.id = data.id;
 });
 
-socket.on("initial_data", function(data) {
-  Object.keys(data.data).forEach(function(key) {
+socket.on("initial_data", function (data) {
+  Object.keys(data.data).forEach(function (key) {
     var member = this[key];
     console.log(member.count);
-    if(member.room == IAM.room){
+    if (member.room == IAM.room) {
       appendAvatar(member.count, member.name);
       moveAvatar(member.count, member.x, member.y);
     }
   }, data.data);
 });
 
-socket.on("s2c_join", function(data){
+socket.on("s2c_join", function (data) {
   appendMsg(data.msg);
   appendAvatar(data.id, data.name);
 });
 
-socket.on("s2c_move", function(data){
+socket.on("s2c_move", function (data) {
   moveAvatar(data.id, data.x, data.y);
 });
 
-socket.on("s2c_dist",function(data){
+socket.on("s2c_dist", function (data) {
   drawMsgRange(data.id, data.dist);
 });
 
@@ -57,7 +95,9 @@ socket.on("reconnect", function () {
 });
 
 function appendAvatar(id, name) {
-  $("#field").append(`<div id=${id} class="avatar">${name}<div id=${id}-effect class="avatar-effect"></div></div>`);
+  $("#field").append(
+    `<div id=${id} class="avatar">${name}<div id=${id}-effect class="avatar-effect"></div></div>`
+  );
 }
 
 function appendMsg(text) {
@@ -71,23 +111,26 @@ $("form").submit(function (e) {
   if (IAM.isEnter) {
     // メッセージを送る
     var dist = $("#dist").val();
-    socket.emit("c2s_msg", { token: IAM.token, dist:dist, msg: message });
+    socket.emit("c2s_msg", { token: IAM.token, dist: dist, msg: message });
   } else {
     IAM.name = message;
     IAM.room = $("#rooms").val();
-    socket.emit("c2s_join", {token: IAM.token, name: IAM.name, room: selectRoom });
+    socket.emit("c2s_join", {
+      token: IAM.token,
+      name: IAM.name,
+      room: selectRoom,
+    });
     changeLabel();
   }
   e.preventDefault();
 });
 
-$("#field").click(function(e){
+$("#field").click(function (e) {
   var offset = $(this).offset();
   var x = e.pageX - offset.left;
   var y = e.pageY - offset.top;
-  socket.emit("c2s_move", { token:IAM.token, x:x , y:y });
+  socket.emit("c2s_move", { token: IAM.token, x: x, y: y });
 });
-
 
 function changeLabel() {
   IAM.isEnter = true;
@@ -96,56 +139,56 @@ function changeLabel() {
   $(".form-pass").remove();
   $("#rooms").prop("disabled", true);
   $("button").text("送信");
-  $(".form-group").append('<label class="nameLabel" for="dist">伝わる距離</label>\n<input type="range" id="dist" min="0" max="1500" value="80" step="5">');
+  $(".form-group").append(
+    '<label class="nameLabel" for="dist">伝わる距離</label>\n<input type="range" id="dist" min="0" max="1500" value="80" step="5">'
+  );
   $("#dist").change(drawCurrentDist);
 }
 
-function moveAvatar(id, x, y){
+function moveAvatar(id, x, y) {
   const avatar = document.getElementById(id);
   anime({
     targets: avatar,
     translateX: x,
     translateY: y,
-    easing: 'linear'
+    easing: "linear",
   });
 }
 
-function drawMsgRange(id, dist){
+function drawMsgRange(id, dist) {
   const avatar = document.getElementById(id);
   const effect = $(`${IAM.id}-effect`);
-  var scale = dist/$(`#${id}`).height();
+  var scale = dist / $(`#${id}`).height();
   var tl = anime.timeline({
-    duration: 500
+    duration: 500,
   });
-  tl
-    .add({
-      targets: avatar,
-      scale: scale,
-      opacity: 0.3,
-      easing: 'linear'
-    })
-    .add({
-      targets: avatar,
-      scale: 1,
-      opacity: 1,
-      easing: 'easeInElastic(1, .6)'
-    });
+  tl.add({
+    targets: avatar,
+    scale: scale,
+    opacity: 0.3,
+    easing: "linear",
+  }).add({
+    targets: avatar,
+    scale: 1,
+    opacity: 1,
+    easing: "easeInElastic(1, .6)",
+  });
 }
 
-function drawCurrentDist(){
+function drawCurrentDist() {
   var height = $(`#${IAM.id}`).height();
   var dist = $("#dist").val();
   console.log($("#dist").val());
-  var fix = height/2;
+  var fix = height / 2;
   const effect = document.getElementById(`${IAM.id}-effect`);
   anime({
-    backgroundColor: 'rgba(255, 0, 0, .2)',
+    backgroundColor: "rgba(255, 0, 0, .2)",
     targets: effect,
     height: dist,
     width: dist,
-    borderRadius: dist/2,
+    borderRadius: dist / 2,
     translateX: "+=fix",
     translateY: "+=fix",
-    easing: 'linear'
+    easing: "linear",
   });
 }
--- serve.js
+++ serve.js
@@ -1,8 +1,19 @@
-const express  = require("express");
-const app  = express();
+const express = require("express");
+const app = express();
+const { pool } = require("./dbConfig");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const flash = require("express-flash");
+const passport = require("passport");
+
+const initializePassport = require("./passportConfig");
+
+initializePassport(passport);
+
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const crypto = require("crypto");
+const { report } = require("process");
 const DOCUMENT_ROOT = __dirname + "/public";
 //デプロイ時は変更の上環境変数にして削除
 const SECRET_TOKEN = "abcdefghijklmn12345";
@@ -11,63 +22,208 @@ const MEMBER = {};
 const TOKENS = {};
 let MEMBER_COUNT = 1;
 
-app.get("/", (req, res)=>{
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: "secret",
+
+    resave: false,
+
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+app.get("/", (req, res) => {
   res.sendFile(DOCUMENT_ROOT + "/index.html");
 });
+
+app.get("/users/register", checkAuthenticated, (req, res) => {
+  res.render("register");
+});
+
+app.get("/users/login", checkAuthenticated, (req, res) => {
+  res.render("login");
+});
+
+app.get("/users/dashboard", checkNotAutheticated, (req, res) => {
+  res.render("dashboard", { user: req.user.name });
+});
+
+app.get("/users/logout", (req, res) => {
+  req.logout();
+  req.flash("success_msg", "ログアウトを完了しました");
+  res.redirect("/users/login");
+});
+
+app.post("/users/register", async (req, res) => {
+  let { name, email, password, password2 } = req.body;
+
+  console.log({
+    name,
+    email,
+    password,
+    password2,
+  });
+
+  let errors = [];
+
+  if (!name || !email || !password || !password2) {
+    errors.push({ message: "すべての項目を入力してください" });
+  }
+
+  if (password.length < 6) {
+    errors.push({ message: "パスワードは最低6文字にしてください" });
+  }
+
+  if (password != password2) {
+    errors.push({ message: "パスワードが一致しません" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", { errors });
+  } else {
+    let hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+
+    pool.query(
+      `SELECT * FROM users
+        WHERE email = $1`,
+      [email],
+      (err, results) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(results.rows);
+
+        if (results.rows.length > 0) {
+          errors.push({ message: "このメールアドレスは既に登録されています" });
+          res.render("register", { errors });
+        } else {
+          pool.query(
+            `INSERT INTO users (name, email, password)
+            VALUES ($1, $2, $3)
+            RETURNING id, password`,
+            [name, email, hashedPassword],
+            (err, results) => {
+              if (err) {
+                throw err;
+              }
+              console.log(results.rows);
+              req.flash(
+                "success_msg",
+                "登録が完了しました。ログインしてください。"
+              );
+              res.redirect("/users/login");
+            }
+          );
+        }
+      }
+    );
+  }
+});
+
+app.post(
+  "/users/login",
+  passport.authenticate("local", {
+    successRedirect: "/users/dashboard",
+    failureRedirect: "/users/login",
+    failureFlash: true,
+  })
+);
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/dashboard");
+  }
+  next();
+}
+
+function checkNotAutheticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/users/login");
+}
 
 app.use(express.static(__dirname + "/public"));
 app.use("/animejs", express.static(__dirname + "/node_modules/animejs/lib/"));
 
 // S04. connectionイベントを受信する
 io.on("connection", function (socket) {
-
-    (()=>{
+  (() => {
     // トークンを作成
-    const token = crypto.createHash("sha1").update(SECRET_TOKEN + socket.id).digest('hex');
+    const token = crypto
+      .createHash("sha1")
+      .update(SECRET_TOKEN + socket.id)
+      .digest("hex");
 
     // ユーザーリストに追加
-    MEMBER[socket.id] = {name:null, room:null, count:MEMBER_COUNT, x: 0, y: 0};
+    MEMBER[socket.id] = {
+      name: null,
+      room: null,
+      count: MEMBER_COUNT,
+      x: 0,
+      y: 0,
+    };
     TOKENS[socket.id] = token;
     MEMBER_COUNT++;
 
     // 本人にトークンを送付
-    io.to(socket.id).emit("token", { token: token, id:MEMBER[socket.id].count });
+    io.to(socket.id).emit("token", {
+      token: token,
+      id: MEMBER[socket.id].count,
+    });
     console.log(socket.id);
   })();
 
   // ルームに入室されたらsocketをroomにjoinさせてメンバーリストにもそれを反映
   socket.on("c2s_join", function (data) {
-    if(data.token == TOKENS[socket.id]){
+    if (data.token == TOKENS[socket.id]) {
       io.to(socket.id).emit("initial_data", { data: MEMBER });
       console.log(MEMBER);
       MEMBER[socket.id].name = data.name;
       MEMBER[socket.id].room = data.room;
       socket.join(data.room);
       var msg = MEMBER[socket.id].name + "さんが入室しました。";
-      io.to(MEMBER[socket.id].room).emit("s2c_join", { id: MEMBER[socket.id].count, name: MEMBER[socket.id].name, msg: msg });
+      io.to(MEMBER[socket.id].room).emit("s2c_join", {
+        id: MEMBER[socket.id].count,
+        name: MEMBER[socket.id].name,
+        msg: msg,
+      });
     }
   });
   // メッセージがきたら名前とメッセージをくっつけて送り返す
   socket.on("c2s_msg", function (data) {
     // S06. server_to_clientイベント・データを送信する
     var minDist = data.dist;
-    if(TOKENS[socket.id] == data.token){
+    if (TOKENS[socket.id] == data.token) {
       var sender = MEMBER[socket.id];
       io.to(sender.room).emit("s2c_dist", { id: sender.count, dist: minDist });
       var msg = sender.name + ": " + data.msg;
-      Object.keys(MEMBER).forEach(function(key) {
+      Object.keys(MEMBER).forEach(function (key) {
         var member = MEMBER[key];
         var dist = calcDist(member.x, member.y, sender.x, sender.y);
-        if(dist<minDist)io.to(key).emit("s2c_msg", { msg: msg });
+        if (dist < minDist) io.to(key).emit("s2c_msg", { msg: msg });
       }, MEMBER);
     }
   });
 
-  socket.on("c2s_move", function(data){
-    if(TOKENS[socket.id] == data.token){
+  socket.on("c2s_move", function (data) {
+    if (TOKENS[socket.id] == data.token) {
       MEMBER[socket.id].x = data.x;
       MEMBER[socket.id].y = data.y;
-      io.to(MEMBER[socket.id].room).emit("s2c_move", { id: MEMBER[socket.id].count, x:MEMBER[socket.id].x, y:MEMBER[socket.id].y });
+      io.to(MEMBER[socket.id].room).emit("s2c_move", {
+        id: MEMBER[socket.id].count,
+        x: MEMBER[socket.id].x,
+        y: MEMBER[socket.id].y,
+      });
     }
   });
 
@@ -77,19 +233,22 @@ io.on("connection", function (socket) {
       console.log("未入室のまま、どこかへ去っていきました。");
     } else {
       var msg = MEMBER[socket.id].name + "さんが退出しました。";
-      io.to(MEMBER[socket.id].room).emit("s2c_leave", { id:MEMBER[socket.id].count, msg: msg });
+      io.to(MEMBER[socket.id].room).emit("s2c_leave", {
+        id: MEMBER[socket.id].count,
+        msg: msg,
+      });
     }
     delete MEMBER[socket.id];
   });
   socket.on("user-reconnected", function (data) {
-    console.log(data+"recconected");
+    console.log(data + "recconected");
   });
 });
 
-function calcDist(x1, y1, x2, y2){
-  return Math.sqrt((x1-x2) * (x1-x2) + (y1-y2) * (y1-y2));
+function calcDist(x1, y1, x2, y2) {
+  return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 }
 
-http.listen(3000, function(){
-    console.log("listening on *:3000");
-})
+http.listen(3000, function () {
+  console.log("listening on *:3000");
+});
